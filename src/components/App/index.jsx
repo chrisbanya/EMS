4b1a3260c404028f4303d9689bc31d339a843c17
Adabanya Christian <adabanyachristian21@gmail.com>
2025-03-11 18:35:14 +0100

    initial commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from '../Login/index';
+import Dashboard from '../Dashboard/index';
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    setIsAuthenticated(JSON.parse(localStorage.getItem('is_authenticated')));
+  }, []);
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Dashboard setIsAuthenticated={setIsAuthenticated} />
+      ) : (
+        <Login setIsAuthenticated={setIsAuthenticated} />
+      )}
+    </>
+  );
+};
+
+export default App;
